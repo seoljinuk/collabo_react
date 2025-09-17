a68bc6ca859dfbda4e724a06ef7c3026a2db24f4
bluesky <bluesky@example.com>
2025-09-17 17:54:01 +0900

    collabo_react 2025/09/17

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-
 import './App.css';
 
 // 분리된 리액트 컴포넌트 import
-import MenuItems from './ui/MenuItems' ;
-import AppRoutes from './routes/AppRoutes' ;
+import MenuItems from './ui/MenuItems';
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   const appName = "IT Academy Coffee Shop";
 
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href='/'>{appName}</Navbar.Brand>
-          <Nav className="me-auto">
-            <MenuItems />
-          </Nav>
-        </Container>
-      </Navbar>
+      <MenuItems appName={appName}/>
 
       {/* 분리된 라우터 정보 */}
-      <AppRoutes />      
+      <AppRoutes />
 
       <footer className="bg-dark text-light text-center py-3 mt-5">
         <p>&copy; 2025 {appName}. All rights reserved.</p>
