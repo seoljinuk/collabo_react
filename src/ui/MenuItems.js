752654acda28682f205908ee010d6cad2e74f994
bluesky <bluesky@example.com>
2025-09-18 17:09:00 +0900

    collabo_react 2025/07/18

--- src/ui/MenuItems.js
+++ src/ui/MenuItems.js
@@ -13,6 +13,7 @@ function App({ appName }) {
                 <Nav className="me-auto">
                     {/* 하이퍼링크 : Nav.Link는 다른 페이지로 이동할 때 사용됩니다.  */}
                     <Nav.Link>상품 보기</Nav.Link>
+                    <Nav.Link onClick={() => navigate(`/member/signup`)}>회원 가입</Nav.Link>
                     <NavDropdown title={`기본 연습`}>
                         <NavDropdown.Item onClick={() => navigate(`/fruit`)}>과일 1개</NavDropdown.Item>
                         <NavDropdown.Item onClick={() => navigate(`/fruit/list`)}>과일 목록</NavDropdown.Item>
